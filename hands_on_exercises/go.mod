ae7fcf2675014e3c018dadc5c329059f9034e489
rob horner <rhorner@totalwine.com>
2024-04-11 14:29:58 -0400

    add puppy dependency

--- hands_on_exercises/go.mod
+++ hands_on_exercises/go.mod
@@ -1,3 +1,7 @@
 module hands-on-exercises
 
 go 1.22.1
+
+require github.com/GoesToEleven/puppy v1.2.0
+
+require github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9 // indirect
--- hands_on_exercises/main.go
+++ hands_on_exercises/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	handson "hands-on-exercises/handsons"
+
+	"github.com/GoesToEleven/puppy"
 )
 
 func main() {
 	fmt.Println(handson.Foo)
 	fmt.Println(handson.Bar)
+
+	fmt.Println(puppy.Bark())
 }
