c9e36a0d10d2beba7d6a7cb4aa57b90ddb805a60
rho <rdhjr81@gmail.com>
2024-07-21 12:38:45 -0400

    finish exercises

--- stucts/main.go
+++ stucts/main.go
@@ -34,7 +34,7 @@ func Ex53() {
 	}
 }
 
-func Ex54(){
+func Ex54() {
 	type person struct {
 		firstName       string
 		lastName        string
@@ -53,9 +53,89 @@ func Ex54(){
 		favoriteFlavors: []string{"vanilla", "orange dreamsicle"},
 	}
 
-	personMap := map[string, person]
+	personMap := map[string]person{}
+	personMap[p1.lastName] = p1
+	personMap[p2.lastName] = p2
+
+	for _, v := range personMap {
+		fmt.Println(v.firstName, " ", v.lastName)
+		fmt.Println("flavors: ")
+		for _, v := range v.favoriteFlavors {
+			fmt.Print(v + " ")
+		}
+		fmt.Println()
+	}
+}
+
+func Ex55() {
+	type engine struct {
+		electric bool
+	}
+
+	type vehicle struct {
+		engine
+		make  string
+		model string
+		doors int
+		color string
+	}
+
+	prius := vehicle{
+		engine: engine{electric: true},
+		make:   "toyota",
+		model:  "prius",
+		doors:  4,
+		color:  "red",
+	}
+
+	f150 := vehicle{
+		engine: engine{
+			electric: true,
+		},
+		make:  "hyundia",
+		model: "ion",
+		doors: 4,
+		color: "silver",
+	}
+	fmt.Println(prius)
+	fmt.Println(f150)
+
+}
+
+func Ex56() {
+	p1 := struct {
+		first     string
+		friends   map[string]int
+		favDrinks []string
+	}{
+		first: "arnold",
+		friends: map[string]int{
+			"alex": 43,
+			"joe":  23,
+		},
+		favDrinks: []string{"beer", "light beer"},
+	}
+
+	fmt.Println(p1)
+
+	p2 := struct {
+		first     string
+		friends   map[string]int
+		favDrinks []string
+	}{
+		first: "pete",
+		friends: map[string]int{
+			"josh": 45,
+			"joe":  78,
+		},
+		favDrinks: []string{"pina colada", "rose"},
+	}
+
+	fmt.Println(p2)
 }
 func main() {
-	Ex53()
-
+	// Ex53()
+	// Ex54()
+	// Ex55()
+	Ex56()
 }
